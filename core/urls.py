917ad814e3adadec0c39545dced3cf493b31824b
sujpac <sujit.pack@gmail.com>
2021-04-21 18:16:44 -0700

    added features to order-summary page: decrement & increment item quantity, trash the item from the order

--- core/urls.py
+++ core/urls.py
@@ -5,7 +5,8 @@ from .views import (
     HomeView,
     OrderSummaryView,
     add_to_cart,
-    remove_from_cart
+    remove_from_cart,
+    remove_single_item_from_cart
 )
 
 
@@ -17,5 +18,8 @@ urlpatterns = [
     path('order-summary', OrderSummaryView.as_view(), name='order-summary'),
     path('product/<slug>/', ItemDetailView.as_view(), name='product'),
     path('add-to-cart/<slug>/', add_to_cart, name='add-to-cart'),
-    path('remove-from-cart/<slug>/', remove_from_cart, name='remove-from-cart')
+    path('remove-from-cart/<slug>/',
+         remove_from_cart, name='remove-from-cart'),
+    path('remove-single-item-from-cart/<slug>/',
+         remove_single_item_from_cart, name='remove-single-item-from-cart')
 ]
